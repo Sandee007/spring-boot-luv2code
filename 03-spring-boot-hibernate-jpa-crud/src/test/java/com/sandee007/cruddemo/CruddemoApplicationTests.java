5677c61830079349bce3ef601637588300055b9b
Sandee007 <sandeepaalahakoon@gmail.com>
2023-08-01 21:28:26 +0530

    define entity class

---
+++ 03-spring-boot-hibernate-jpa-crud/src/test/java/com/sandee007/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandee007.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
