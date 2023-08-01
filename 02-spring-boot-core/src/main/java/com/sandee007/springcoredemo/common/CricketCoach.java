6740615944cde3fddc99817d459c1050d5b80f06
Sandee007 <sandeepaalahakoon@gmail.com>
2023-08-01 18:59:18 +0530

    chapter02-05-primary

--- 02-spring-boot-core/src/main/java/com/sandee007/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-core/src/main/java/com/sandee007/springcoredemo/common/CricketCoach.java
@@ -1,8 +1,10 @@
 package com.sandee007.springcoredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component // @Component annotation marks the class as a spring bean - makes it available for dependency injection
+@Primary // @Qualifier will overwrite @Primary if available
 public class CricketCoach implements Coach {
     @Override
     public String getDailyWorkout() {
