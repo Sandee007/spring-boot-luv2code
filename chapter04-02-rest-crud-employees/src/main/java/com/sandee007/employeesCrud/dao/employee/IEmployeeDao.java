7bd35a85fe22232320374c97fba8723c450f9b09
Sandee007 <sandeepaalahakoon@gmail.com>
2023-08-02 22:36:06 +0530

    chapter04-02-rest-crud-employees-sping-data-jpa

--- chapter04-02-rest-crud-employees/src/main/java/com/sandee007/employeesCrud/dao/employee/IEmployeeDao.java
+++
@@ -1,8 +0,0 @@
-package com.sandee007.employeesCrud.dao.employee;
-
-import com.sandee007.employeesCrud.dao.global.IGlobalDao;
-import com.sandee007.employeesCrud.entity.Employee;
-
-public interface IEmployeeDao extends IGlobalDao<Employee> {
-
-}
--- chapter04-02-rest-crud-employees/src/main/java/com/sandee007/employeesCrud/service/employee/EmployeeService.java
+++ chapter04-02-rest-crud-employees/src/main/java/com/sandee007/employeesCrud/service/employee/EmployeeService.java
@@ -1,41 +1,48 @@
 package com.sandee007.employeesCrud.service.employee;
 
-import com.sandee007.employeesCrud.dao.employee.EmployeeDao;
+import com.sandee007.employeesCrud.dao.IEmployeeRepository;
 import com.sandee007.employeesCrud.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService implements IEmployeeService {
-    private EmployeeDao employeeDao;
+    private IEmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeService(EmployeeDao employeeDao) {
-        this.employeeDao = employeeDao;
+    public EmployeeService(IEmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDao.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int id) {
-        return employeeDao.findById(id);
+        Optional<Employee> result = employeeRepository.findById(id);
+
+        Employee employee = null;
+        if(result.isPresent()){
+            employee = result.get();
+        }else{
+            throw new RuntimeException("Not found");
+        }
+        return employee;
     }
 
-    @Transactional
     @Override
     public Employee save(Employee payload) {
-        return employeeDao.save(payload);
+        return employeeRepository.save(payload);
     }
 
-    @Transactional
     @Override
     public void deleteById(int id) {
-        employeeDao.deleteById(id);
+        employeeRepository.deleteById(id);
     }
 }
