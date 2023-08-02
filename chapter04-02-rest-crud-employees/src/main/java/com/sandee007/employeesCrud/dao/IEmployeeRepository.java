7b747fc27bb26d7b8ef0f25ced33f8db9a8eb398
Sandee007 <sandeepaalahakoon@gmail.com>
2023-08-02 23:05:14 +0530

    chapter04-02-rest-crud-employees-spring-data-REST

--- chapter04-02-rest-crud-employees/src/main/java/com/sandee007/employeesCrud/dao/IEmployeeRepository.java
+++ chapter04-02-rest-crud-employees/src/main/java/com/sandee007/employeesCrud/dao/IEmployeeRepository.java
@@ -2,6 +2,8 @@ package com.sandee007.employeesCrud.dao;
 
 import com.sandee007.employeesCrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
 public interface IEmployeeRepository extends JpaRepository<Employee, Integer> {
 }
