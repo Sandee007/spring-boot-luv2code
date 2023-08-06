1db20e6c92ab2c0cf395872aac53e5a091d1b54b
Sandee007 <sandeepaalahakoon@gmail.com>
2023-08-06 13:44:17 +0530

    initial setup

---
+++ 09-hibernate-advanced-mappings/src/main/java/com/sandee007/hibernateAdvancedMappings/HibernateAdvancedMappingsApplication.java
@@ -0,0 +1,22 @@
+package com.sandee007.hibernateAdvancedMappings;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HibernateAdvancedMappingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateAdvancedMappingsApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return runner -> {
+			System.out.println("Hello w");
+		};
+	}
+}
